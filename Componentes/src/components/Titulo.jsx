c55d79ab2569bc0bc9b14bc3f7a6689fdd7ca0cb
andre-smedina <andrelseu@gmail.com>
2023-09-07 13:40:50 +0000

    add

---
+++ Componentes/src/components/Titulo.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Titulo(obj) {
+  // para utilizar o useState, deve criar um array com uma variável e uma função, atribuindo a ambas o valor que está na função useState(), que nesse caso é zero.
+  let [salario, setSalario] = useState(0);
+  return (
+    <div>
+      <h2 style={{ color: obj.cor }}>
+        {obj.nome} tem {obj.idade} anos e recebe R${" "}
+        {obj.salario ? obj.salario : salario},00{" "}
+      </h2>
+      <button
+        onClick={() => {
+          salario += 100;
+          //aqui no botão, é passado o novo valor de salario para a função setSalario, que será atualizado na página.
+          setSalario(salario);
+        }}
+      >
+        + Salário
+      </button>
+      <input
+        type="text"
+        onChange={(e) => setSalario(parseInt(e.target.value))}
+        placeholder="Valor do salário"
+      />
+      {obj.children}
+    </div>
+  );
+}
+
+export default Titulo;
