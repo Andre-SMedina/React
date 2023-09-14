3ba95de8f79ea9e6f4ebe6e47c9cde1868787bf4
andre <andre-smedina>
2023-09-13 21:24:47 -0300

    add

---
+++ Curso_bootcamp/Project1/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = "3333";
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Bem vindos!!" });
+});
+
+app.listen(port, () => {
+  console.log("Rodando na porta " + port);
+});
